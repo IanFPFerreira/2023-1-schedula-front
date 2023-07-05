ac2184455ee6d11c77df8362957f3aa233f73f40
IanFPFerreira <ianfillipe@gmail.com>
2023-07-05 19:55:41 -0300

    id/develop_2023a06a24T02p32p50Z

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,145 @@
+import { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { Box, Button, Center, Input, Text, Flex } from '@chakra-ui/react';
+import { CalendarClock } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '@/contexts/AuthContext';
+
+export function Login() {
+  const { signIn } = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CredentialUser>();
+
+  const onSubmit: SubmitHandler<CredentialUser> = async ({
+    username,
+    password,
+  }) => {
+    setIsLoading(true);
+    await signIn({ username, password });
+    setIsLoading(false);
+  };
+
+  return (
+    <Center
+      aria-label="form"
+      bgGradient="linear(288.94deg, #F8B86D 0%, #F49320 90.96%)"
+      h="100vh"
+      color="white"
+    >
+      <form onSubmit={handleSubmit(onSubmit)} aria-label="form">
+        <Box
+          bg="white"
+          borderRadius="10px"
+          boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25),  0px 4px 4px rgba(0, 0, 0, 0.25),  0px 1px 1px rgba(0, 0, 0, 0.12),  0px 2px 2px rgba(0, 0, 0, 0.12),  0px 8px 8px rgba(0, 0, 0, 0.12);"
+          color="black"
+          paddingY="20"
+          paddingX="20"
+        >
+          <Text mb="39px" color="#605555" fontWeight="semibold" fontSize="4xl">
+            Bem-vindo
+          </Text>
+          <Box marginBottom={10}>
+            <Text
+              pl="5px"
+              pb="8px"
+              color="#605555"
+              fontWeight="medium"
+              fontSize="lg"
+            >
+              Login
+            </Text>
+            <Input
+              size="lg"
+              fontSize="lg"
+              {...register('username', { required: true })}
+              placeholder="Nome de usuário"
+            />
+            {errors.username && (
+              <span>
+                <Text color="red.400">Este campo é obrigatório</Text>
+              </span>
+            )}
+          </Box>
+          <Box mb="70px">
+            {' '}
+            <Text
+              pl="5px"
+              pb="8px"
+              color="#605555"
+              fontWeight="medium"
+              fontSize="lg"
+            >
+              {' '}
+              Senha{' '}
+            </Text>
+            <Input
+              size="lg"
+              fontSize="lg"
+              {...register('password', { required: true })}
+              type="password"
+              placeholder="Digite sua senha"
+            />
+            {errors.password && (
+              <span>
+                <Text color="red.400">Este campo é obrigatório</Text>
+              </span>
+            )}
+          </Box>
+          <Center>
+            <Button
+              mb="70px"
+              type="submit"
+              paddingX="24"
+              width="sm"
+              isLoading={isLoading}
+            >
+              ENTRAR
+            </Button>
+          </Center>
+        </Box>
+
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+          marginTop="20px"
+        >
+          <Box
+            _hover={{
+              transform: 'scale(1.1)',
+              transition: '0.3s',
+            }}
+            onClick={() => navigate('../agendamentos_abertos')}
+            cursor="pointer"
+          >
+            <CalendarClock fontSize={18} size={70} />
+          </Box>
+
+          <Button
+            onClick={() => navigate('../agendamentos_abertos')}
+            sx={{
+              bgImage:
+                'linear-gradient(to right, #fa781b 0%, #FA652F 51%, #FFA03A 100%)',
+              _hover: {
+                backgroundPosition: 'right center',
+                transition: '0.5s',
+              },
+            }}
+            mt="10px"
+            paddingX="24"
+            width="sm"
+            isLoading={isLoading}
+          >
+            Agendamentos
+          </Button>
+        </Flex>
+      </form>
+    </Center>
+  );
+}
