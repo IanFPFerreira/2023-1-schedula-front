411952458a9575a7cae9eb5f1b38ea7b3ca69ff2
IanFPFerreira <ianfillipe@gmail.com>
2023-07-05 19:50:03 -0300

    id/develop_2023a05a13T19p12p20Z

---
+++ src/pages/chamados/index.tsx
@@ -0,0 +1,42 @@
+import { Button, HStack } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { useCallback } from 'react';
+import { RefreshButton } from '@/components/action-buttons/refresh-button';
+import { PageHeader } from '@/components/page-header';
+import { useGetAllIssues } from '@/features/issues/api/get-all-issues';
+import { Issue } from '@/features/issues/types';
+
+import { ListView } from '@/components/list';
+import { IssueItem } from '@/features/issues/components/issue-item';
+
+export function Chamados() {
+  const {
+    data: issues,
+    isLoading: isLoadingIssues,
+    refetch,
+  } = useGetAllIssues();
+
+  const renderIssueItem = useCallback(
+    (issue: Issue) => <IssueItem issue={issue} />,
+    []
+  );
+
+  return (
+    <>
+      <PageHeader title="Chamados">
+        <HStack spacing={2}>
+          <RefreshButton refresh={refetch} />
+          <Link to="/chamados/registrar">
+            <Button variant="primary">Novo Chamado</Button>
+          </Link>
+        </HStack>
+      </PageHeader>
+
+      <ListView<Issue>
+        items={issues}
+        render={renderIssueItem}
+        isLoading={isLoadingIssues}
+      />
+    </>
+  );
+}
